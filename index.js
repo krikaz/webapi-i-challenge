ac01726ac2f63ca1d020ffd61945debe789c9576
krikaz <pascal.bou@protonmail.com>
2019-07-08 13:35:52 +0200

    added express, created all endpoints

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// implement your API here
+const express = require('express');
+const server = express();
+const Users = require('./data/db');
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+	Users.find()
+		.then(data => {
+			console.log('success');
+			res.json(data);
+		})
+		.catch(error => {
+			console.log('failure');
+			res.json(error);
+		});
+});
+
+server.get('/api/users/:id', (req, res) => {
+	Users.findById(req.params.id)
+		.then(data => {
+			console.log('success');
+			res.json(data);
+		})
+		.catch(error => {
+			console.log('failure');
+			res.json(error);
+		});
+});
+
+server.post('/api/users', (req, res) => {
+	Users.insert(req.body)
+		.then(data => {
+			console.log('success');
+			res.json(data);
+		})
+		.catch(error => {
+			console.log('failure');
+			res.json(error);
+		});
+});
+
+server.put('/api/users', (req, res) => {
+	Users.update(req.params.id, req.body)
+		.then(data => {
+			console.log('success');
+			res.json(data);
+		})
+		.catch(error => {
+			console.log('failure');
+			res.json(error);
+		});
+});
+
+server.delete('/api/users/:id', (req, res) => {
+	Users.remove(req.params.id)
+		.then(data => {
+			console.log('success');
+			res.json(data);
+		})
+		.catch(error => {
+			console.log('failure');
+			res.json(error);
+		});
+});
+
+server.listen(3000, () => {
+	console.log('listening on 3000');
+});
